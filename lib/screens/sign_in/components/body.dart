72b8cc19c6c1dacc35634a002e16e6f45929c61b
Eryk <eryks453@gmail.com>
2021-03-18 18:40:32 +0100

    form fields

--- lib/screens/sign_in/components/body.dart
+++ lib/screens/sign_in/components/body.dart
@@ -5,18 +5,56 @@ class Body extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return SafeArea(
-        child: Column(
+        child: SizedBox(
+          width: double.infinity,
+          child: Padding(
+            padding: EdgeInsets.symmetric(horizontal: getProportionateScreenWidth(20)),
+            child: Column(
       children: [
-        Text(
-          "Welcome",
-          style: TextStyle(
-              color: Colors.black,
-              fontSize: getProportionateScreenWidth(28),
-              fontWeight: FontWeight.bold
-          ),
-        ),
-
+            Text(
+              "Welcome",
+              style: TextStyle(
+                  color: Colors.black,
+                  fontSize: getProportionateScreenWidth(28),
+                  fontWeight: FontWeight.bold
+              ),
+            ),
+            Text(
+                "Sign in with your email and password",
+                textAlign: TextAlign.center,
+            ),
+        SignInForm()
       ],
-    ));
+    ),
+          ),
+        )
+    );
+  }
+}
+
+class SignInForm extends StatefulWidget {
+  @override
+  _SignInFormState createState() => _SignInFormState();
+}
+
+class _SignInFormState extends State<SignInForm> {
+  @override
+  Widget build(BuildContext context) {
+    return Form(
+        child: Column(
+          children: [
+            TextFormField(
+              decoration: InputDecoration(
+                hintText: "Email"
+              ),
+            ),
+            TextFormField(
+              decoration: InputDecoration(
+                  hintText: "Password"
+              ),
+            )
+          ],
+        ),
+    );
   }
 }
